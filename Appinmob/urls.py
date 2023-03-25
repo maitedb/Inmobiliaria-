34a2cee45e78dc62cd75bf0a2ddd886e48e3568a
maitedb <maitedemontebecker@gmail.com>
2023-03-25 11:52:22 -0300

    hasta clase 20

--- Appinmob/urls.py
+++ Appinmob/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from Appinmob.views import inicio, index
+from Appinmob.views import *
 
 urlpatterns = [
-    path('', inicio),
-    path('index/', index),
+    path('', index, name="index"),
+    path('inquilinos/', inquilinos, name="inquilinos"),
+    path('propietarios/', propietarios, name="propietarios"),
+    path('propiedades/', propiedades, name="propiedades"),
+    path('empleados/', empleados, name="empleados"),
 ]
 
--- Appinmob/views.py
+++ Appinmob/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def inicio(request):
-    return HttpResponse("ESTA ES LA PAGINA DE INICIO")
-
 def index(request):
     return render(request, 'Appinmob/index.html')
+
+def inquilinos(request):
+    return render(request, 'Appinmob/inquilinos.html')
+
+def propietarios(request):
+    return render(request, 'Appinmob/propietarios.html')
+
+def propiedades(request):
+    return render(request, 'Appinmob/propiedades.html')
+
+def empleados(request):
+    return render(request, 'Appinmob/empleados.html')
